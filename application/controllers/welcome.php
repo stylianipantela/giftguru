d24dc36578a40e6358a1a8443acec6d03e5d270d
Stella Pantela <stella.pantela@gmail.com>
2014-01-23 16:18:25 -0500

    first commit

---
+++ application/controllers/welcome.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('templates/header', array('title' => 'Home'));
+		$this->load->view('index');
+		$this->load->view('templates/footer');
+	}
+
+	public function about()
+	{
+		$this->load->view('templates/header', array('title' => 'About'));
+		$this->load->view('about');
+		$this->load->view('templates/footer');
+	}
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo htmlspecialchars($title) ?></title>
+<meta name="keywords" content="soft link, about us, our company, free template, templatemo" />
+<meta name="description" content="Soft Link Theme, About Us, Our Company, free web design template by templatemo.com" />
+<link href="/html/css/templatemo_style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="/html/css/ddsmoothmenu.css" />
+<script type="text/javascript" src="/html/js/swfobject/swfobject.js"></script>
+<script type="text/javascript" src="/html/js/jquery.min.js"></script>
+<script type="text/javascript" src="/html/js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+</head>
+
+<body >
