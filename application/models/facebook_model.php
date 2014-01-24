3c49a959e164105eb63e1949925fd7743adb72dd
Stella Pantela <stella.pantela@gmail.com>
2014-01-24 14:54:51 -0500

    facebook redirect_uri added

--- application/models/facebook_model.php
+++ application/models/facebook_model.php
@@ -36,7 +36,7 @@
                         'loginUrl' => $this->facebook->getLoginUrl(
                             array(
                                 'scope' => 'email,user_birthday,publish_stream', // app permissions
-                                'redirect_uri' => '' // URL where you want to redirect your users after a successful login
+                                'redirect_uri' => 'http://giftguru.herokuapp.com/welcome/' // URL where you want to redirect your users after a successful login
                             )
                         ),
                         'logoutUrl' => $this->facebook->getLogoutUrl(),
