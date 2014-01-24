9312d71e476f604a68729453e0012c8e33a0601c
Lili Jiang <lily9393@gmail.com>
2014-01-24 15:57:33 -0500

    fb print facebook data

--- application/views/index.php
+++ application/views/index.php
@@ -6,6 +6,10 @@
 </head>
 <body>
 <div>
+  <?php echo "<pre>";
+  		print_r($fb_data);
+  		echo "</pre>";
+  ?>
   <?php if(!$fb_data['me']): ?>
   Please login with your FB account: <a href="<?php echo $fb_data['loginUrl']; ?>">login</a>
   <?php else: ?>
