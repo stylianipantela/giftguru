42b1bd8044bfe69fb44b8962e61097eec395fdd1
Stella Pantela <stella.pantela@gmail.com>
2014-01-24 02:04:12 -0500

    amazon api setup

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -28,6 +28,32 @@ class Welcome extends CI_Controller {
 		parent::__construct();
 	}
 
+    private function lookup($category, $keyword) {
+
+        $this->load->library('amazon_ecs'); 
+
+        $client = new AmazonECS('AKIAJKTEYNKJYROFSX3Q', 'IlFACjHl3HwTjRnStKy3UQdBGoITnJY/AGATyfBX', 'com', 'munerum-20');
+        // $response  = $client->category($category)->search($keyword);
+        $response = $client->responseGroup('Images,ItemAttributes,OfferSummary')->category($category)->search($keyword);
+        $mystr = "";
+        foreach($response->Items->Item as $current){
+            $mystr .= "<b>".$current->SmallImage->URL."</b><br>";
+            $mystr .= "<b>".$current->DetailPageURL."</b><br>";
+            $mystr .= "<b>".$current->OfferSummary->LowestNewPrice->FormattedPrice."</b><br>";
+            $mystr .= "<b>".$current->ItemAttributes->Title."</b><br>";
+        }    
+        return $mystr;
+    }
+
+
+    public function amazon () {
+        $this->load->library('amazon_ecs');
+        $result = lookup('Books', 'PHP');
+        $this->load->view('templates/header', array('title' => 'Amazon', 'result' => $result));
+        $this->load->view('amazon');
+        $this->load->view('templates/footer');
+    }
+
 	public function search(){
 
 		$this->load->library('facebook'); 
--- application/models/amazon_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once (__DIR__ . "/lib/AmazonECS.class.php";
-	$client = new AmazonECS('YAKIAJKTEYNKJYROFSX3Q', 'IlFACjHl3HwTjRnStKy3UQdBGoITnJY/AGATyfBX', 'US', 'munerum-20');
-
-
-	$response  = $client->category('Books')->search('PHP 5');
-	var_dump($response);
