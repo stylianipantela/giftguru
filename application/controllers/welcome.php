bf05f71ae32f4d7e1c557ebdfe59b42fcdeae40c
Stella Pantela <stella.pantela@gmail.com>
2014-01-24 02:22:08 -0500

    bugfix: amazon

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -30,21 +30,23 @@ class Welcome extends CI_Controller {
 
     private function lookup($category, $keyword) {
         $this->load->library('amazon_ecs'); 
-        $client = new AmazonECS('AKIAJKTEYNKJYROFSX3Q', 'IlFACjHl3HwTjRnStKy3UQdBGoITnJY/AGATyfBX', 'com', 'munerum-20');
-        $response = $client->responseGroup('Images,ItemAttributes,OfferSummary')->category($category)->search($keyword);
         $mystr = "";
-        foreach($response->Items->Item as $current){
-            $mystr .= "<b>".$current->SmallImage->URL."</b><br>";
-            $mystr .= "<b>".$current->DetailPageURL."</b><br>";
-            $mystr .= "<b>".$current->OfferSummary->LowestNewPrice->FormattedPrice."</b><br>";
-            $mystr .= "<b>".$current->ItemAttributes->Title."</b><br>";
-        }    
+        // $client = new AmazonECS('AKIAJKTEYNKJYROFSX3Q', 'IlFACjHl3HwTjRnStKy3UQdBGoITnJY/AGATyfBX', 'com', 'munerum-20');
+        // $response = $client->responseGroup('Images,ItemAttributes,OfferSummary')->category($category)->search($keyword);
+
+        // foreach($response->Items->Item as $current){
+        //     $mystr .= "<b>".$current->SmallImage->URL."</b><br>";
+        //     $mystr .= "<b>".$current->DetailPageURL."</b><br>";
+        //     $mystr .= "<b>".$current->OfferSummary->LowestNewPrice->FormattedPrice."</b><br>";
+        //     $mystr .= "<b>".$current->ItemAttributes->Title."</b><br>";
+        // }    
+
         return $mystr;
     }
 
 
     public function amazon () {
-        // $result = lookup('Books', 'PHP');
+        $result = $this->lookup('Books', 'PHP');
         $this->load->view('templates/header', array('title' => 'Amazon'));
         $this->load->view('amazon');
         $this->load->view('templates/footer');
