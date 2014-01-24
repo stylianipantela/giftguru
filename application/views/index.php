ce42b732136ca68d6da1583cef9684bb3fadf1b9
Lili Jiang <lily9393@gmail.com>
2014-01-24 18:33:18 -0500

    print session id

--- application/views/index.php
+++ application/views/index.php
@@ -6,14 +6,16 @@
 </head>
 <body>
 <div>
-  <?php phpinfo();
-  		echo "<pre>";
+  <?php echo "<pre>";
   		print_r($fb_data);
   		print_r($_GET);
   		print_r($_REQUEST);
   		echo "session";
   		print_r($_SESSION);
   		echo "</pre>";
+  		if(session_id() == '') {
+    		session_start();
+		}
   ?>
   <?php if(!$fb_data['me']): ?>
   Please login with your FB account: <a href="<?php echo $fb_data['loginUrl']; ?>">login</a>
