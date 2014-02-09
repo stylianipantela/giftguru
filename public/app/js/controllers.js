8f2fdaf2899b9233e3a8f78b6eb17cde1f657bd4
Lili Jiang <lily9393@gmail.com>
2014-02-09 04:38:12 -0500

    halfdone dynamic profile page

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -2,8 +2,29 @@
 
 /* Controllers */
 angular.module('myApp.controllers', []).
-  controller('MyProfileCtrl', [function() {
-
+  controller('MyProfileCtrl', ['$scope', '$http', 
+     function($scope,$http){
+      window.jsonp_callback = function(data) {
+        $scope.questions = data.results;
+      }
+      $http.jsonp('http://giftguruapi.herokuapp.com/get_questions/jsonp_callback');
+      
+      window.userid_callback = function(data) {
+        $scope.user_id = data.results;
+      }
+      $http.jsonp('http://giftguruapi.herokuapp.com/get_user/test9393@hotmail.com/userid_callback');
+      
+      window.getAnswer_callback = function(data) {
+        $scope.answers = data.results;
+      }
+      $http.jsonp('http://giftguruapi.herokuapp.com/get_answers/{{user_id}}/userid_callback');
+      
+      $scope.submit = function(answer) {
+        window.setAnswer_callback = function(data) {
+          $scope.set_answer = data.results;
+        }
+        $http.jsonp('http://giftguruapi.herokuapp.com/set_answer/{{user_id}}/1/{{answer}}/setAnswer_callback');
+      }
   }])
   
   .controller('RecResultCtrl', ['$scope', '$routeParams', '$http', 
